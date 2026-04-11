c230abd14cef62997354ff7cc7d7f8684af1de64
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-11 14:58:13 +0530

    new

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
