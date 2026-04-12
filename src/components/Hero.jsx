0fffdef9f80493565bcdf0a50cca6c3ce51c1d89
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-12 14:57:30 +0530

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import "./Hero.css";
 import Navbar from "./Navbar";
 
 const Hero = () => {
+  const heroRef = useRef(null);
+  const navbarVariant = {
+    hidden: { y: -100, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: { type: "spring", stiffness: 80, damping: 14 },
+    },
+  };
+
   return (
     <div className="hero" ref={heroRef}>
-      <motion.div>
+      <motion.div variants={navbarVariant} initial="hidden" animate="visible">
         <Navbar />
       </motion.div>
     </div>
