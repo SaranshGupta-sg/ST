7c3c590c882093522258b0765e2ada23863b0b8d
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-12 14:49:17 +0530

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import './Hero.css'
-import Navbar from './Navbar'
+import React from "react";
+import { motion } from "framer-motion";
+import "./Hero.css";
+import Navbar from "./Navbar";
 
 const Hero = () => {
   return (
-    <div>
+    <div className="hero" ref={heroRef}>
       <motion.div>
         <Navbar />
       </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
